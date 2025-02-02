cf4253a48f01d662f26fb4df4eb6ed6b5e55d925
sebber925 <63114738+sebber925@users.noreply.github.com>
2025-02-02 21:48:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+function getComputerChoice() {
+
+};
+
+
+// I want a ComputerChoice, where the result gets chosen random out of 3 options: Rock, Paper, Scissors
+// Then I want a human input, fill in a prompt where you choose an option.
